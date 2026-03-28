5cc3bcb00829918c0671a2f12f5bc73e6e4cdd8b
PranixQuick <pranixailabs@gmail.com>
2026-03-28 20:10:03 +0530

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -213,4 +213,4 @@ export default function DashboardPage() {
       </div>
     </Layout>
   );
-                                                                                                                                           }
+}
